e353d145f75a0ca8243767fc21958a26b1a4d3cd
niksaz <nikitasazanovich@gmail.com>
2018-10-07 13:24:28 +0300

    Basic TensorForce Dota learning agent.

--- tensorforce/dota.py
+++ tensorforce/dota.py
@@ -1,17 +1,31 @@
 import pickle
+import sys
+
+sys.path.append('../')
+
+from dotaenv import DotaEnvironment
 
 import matplotlib.pyplot as plt
 from tensorforce.agents import PPOAgent
-from tensorforce.contrib.openai_gym import OpenAIGym
 from tensorforce.execution import Runner
 
-# Create an OpenAI gym environment
-env = OpenAIGym('CartPole-v0', visualize=True)
 
-# Network as list of layers
+reload = False
+if reload:
+    with open('saved_rewards.pkl', 'rb') as output_file:
+        rewards = pickle.load(output_file)
+else:
+    rewards = []
+
+# Create a Dota environment
+env = DotaEnvironment()
+
+# Network
 network_spec = [
-    dict(type='dense', size=32, activation='tanh'),
-    dict(type='dense', size=32, activation='tanh')
+    dict(type='dense', size=9),
+    dict(type='nonlinearity', name='relu'),
+    dict(type='dense', size=16),
+    dict(type='nonlinearity', name='relu')
 ]
 
 agent = PPOAgent(
@@ -30,6 +44,10 @@ agent = PPOAgent(
     # Model
     scope='ppo',
     discount=0.99,
+    saver=dict(
+        directory='weights/',
+        load=reload
+    ),
     # DistributionModel
     distributions=None,
     entropy_regularization=0.01,
@@ -48,22 +66,23 @@ runner = Runner(agent=agent, environment=env)
 
 # Callback function printing episode statistics
 def episode_finished(r):
-    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.episode_timestep,
-                                                                                 reward=r.episode_rewards[-1]))
+    reward = r.episode_rewards[-1]
+    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode,
+                                                                                 ts=r.episode_timestep,
+                                                                                 reward=reward))
+    rewards.append(reward)
     with open('saved_rewards.pkl', 'wb') as output_file:
-        pickle.dump(obj=r.episode_rewards, file=output_file)
+        pickle.dump(obj=rewards, file=output_file)
 
     return True
 
 
 # Start learning
-runner.run(episodes=5, max_episode_timesteps=200,
+runner.run(episodes=200, max_episode_timesteps=10000,
            episode_finished=episode_finished)
 runner.close()
 
-plt.plot(
-    list(map(lambda x: 2*x, range(len(runner.episode_rewards)))),
-runner.episode_rewards)
+plt.plot(runner.episode_rewards)
 plt.title('Reward per episode')
 plt.xlabel('Episode')
 plt.ylabel('Reward')
