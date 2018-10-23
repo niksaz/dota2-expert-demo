88e07856055aef9cfac41aabf67b0da55cf4553a
niksaz <nikitasazanovich@gmail.com>
2018-10-24 00:08:17 +0300

    State preprocessing with exact world bounds.

--- deepq/state_preprocessor.py
+++ deepq/state_preprocessor.py
@@ -6,12 +6,12 @@ class StatePreprocessor:
     @staticmethod
     def process(state):
         if len(state) > 0:
-            # Normalize x position from [-10000; 10000] to [0, 1]
-            state[0] = (state[0] - (-10000)) / 20000
+            # Normalize x position from [-8288; 8288] to [-1, 1]
+            state[0] = state[0] / 8288
         if len(state) > 1:
-            # Normalize y position from [-10000; 10000] to [0, 1]
-            state[1] = (state[1] - (-10000)) / 20000
+            # Normalize y position from [-8288; 8288] to [-1, 1]
+            state[1] = state[1] / 8288
         if len(state) > 2:
-            # Normalize facing from [0; 360] to [0, 1]
-            state[2] = state[2] / 360
+            # Normalize facing from [0; 360] to [-1, 1]
+            state[2] = (state[2] - 180) / 180
         return state
