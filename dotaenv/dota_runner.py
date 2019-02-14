61ebd917ff8978ac418cf79ec0ba0577546d3c8d
niksaz <nikitasazanovich@gmail.com>
2019-02-14 14:16:14 +0300

    GUI config is detached from the code.

--- dotaenv/dota_runner.py
+++ dotaenv/dota_runner.py
@@ -1,26 +1,42 @@
 import time
+import json
 import subprocess
 import pyautogui as gui
 
+with open('gui_config.json', 'r') as finput:
+    config = json.load(finput)
+print('GUI config', config)
 
-RIGHT_BOT_BUTTON_X = 906
-RIGHT_BOT_BUTTON_Y = 809
+STEAM_LIBRARY = config['steam_library_button']
+STEAM_SEARCH = config['steam_search_box']
+STEAM_PLAY = config['steam_play_button']
+STEAM_MENU = config['steam_menu_option']
+STEAM_EXIT = config['steam_exit_option']
 
-INTERVAL = 0.01
-DURATION = 0.5
-PAUSE = 1
+RIGHT_BOT_BUTTON = config['dota_right_bottom_button']
+LEAVE_BUTTON = config['dota_leave_game_button']
+MENU_CONFIRM_BUTTON = config['dota_menu_confirm_button']
+CREATE_LOBBY_BUTTON = config['dota_create_lobby_button']
+JOIN_COACHES_BUTTON = config['dota_join_coaches_button']
+INGAME_MENU_BUTTON = config['dota_ingame_menu_button']
+INGAME_CONFIRM_BUTTON = config['dota_ingame_confirm_button']
+EXIT_BUTTON = config['dota_exit_button']
 
-restart_episode_called = 0
-RESTART_AFTER_EPISODES = 100
+TYPEWRITE_INT = config['typewrite_interval']
+MOUSE_DURATION = config['mouse_movement_duration']
+PAUSE = config['in_between_pause']
+
+RESTART_AFTER_EPISODES = config['restart_client_every_nth']
+
+episodes_since_last_restart = 0
 
 
 def prepare_steam_client():
     if _is_steam_launched():
         _focus_steam_window()
     else:
-        # "Search your computer" Ubuntu function
-        gui.click(x=32, y=56)
-        gui.typewrite('steam', interval=INTERVAL, pause=PAUSE)
+        gui.press('winleft', pause=PAUSE)
+        gui.typewrite('steam', interval=TYPEWRITE_INT, pause=PAUSE)
 
         # Run the first option
         gui.press('enter', pause=PAUSE)
@@ -31,39 +47,33 @@ def prepare_dota_client():
     if _is_dota_launched():
         _focus_dota_window()
     else:
-        # Click on the library option
-        gui.click(x=307, y=85, pause=PAUSE)
-        # Search for Dota 2 in the library
-        gui.click(x=150, y=120)
-        gui.typewrite('dota', interval=INTERVAL)
+        gui.click(x=STEAM_LIBRARY['x'], y=STEAM_LIBRARY['y'], pause=PAUSE)
+        gui.click(x=STEAM_SEARCH['x'], y=STEAM_SEARCH['y'])
+        gui.typewrite('dota', interval=TYPEWRITE_INT)
 
-        # Press play
-        gui.click(x=416, y=236, pause=30)
+        gui.click(x=STEAM_PLAY['x'], y=STEAM_PLAY['y'], pause=30)
         calibrate_dota_client()
         start_game()
 
 
 def start_game():
     # Leave the game if there is one
-    gui.click(x=832, y=786, duration=DURATION, pause=PAUSE)
-    # Confirm it
-    gui.click(x=580, y=571, pause=4*PAUSE)
+    gui.click(x=LEAVE_BUTTON['x'], y=LEAVE_BUTTON['y'], duration=MOUSE_DURATION, pause=PAUSE)
+    gui.click(x=MENU_CONFIRM_BUTTON['x'], y=MENU_CONFIRM_BUTTON['y'], pause=4*PAUSE)
 
     # Start
-    gui.click(x=RIGHT_BOT_BUTTON_X, y=RIGHT_BOT_BUTTON_Y, duration=DURATION, pause=PAUSE)
-    # Create lobby
-    gui.click(x=854, y=424, duration=DURATION, pause=PAUSE)
-    # Join coaches
-    gui.click(x=807, y=484, duration=DURATION, pause=PAUSE)
-    # Start game
-    gui.click(x=RIGHT_BOT_BUTTON_X, y=RIGHT_BOT_BUTTON_Y, duration=DURATION, pause=PAUSE)
+    gui.click(x=RIGHT_BOT_BUTTON['x'], y=RIGHT_BOT_BUTTON['y'], duration=MOUSE_DURATION, pause=PAUSE)
+    gui.click(x=CREATE_LOBBY_BUTTON['x'], y=CREATE_LOBBY_BUTTON['y'], duration=MOUSE_DURATION, pause=PAUSE)
+    gui.click(x=JOIN_COACHES_BUTTON['x'], y=JOIN_COACHES_BUTTON['y'], duration=MOUSE_DURATION, pause=PAUSE)
+    # Start the game
+    gui.click(x=RIGHT_BOT_BUTTON['x'], y=RIGHT_BOT_BUTTON['y'], duration=MOUSE_DURATION, pause=PAUSE)
 
 
 def restart_game():
-    global restart_episode_called
-    restart_episode_called += 1
-    if restart_episode_called >= RESTART_AFTER_EPISODES:
-        restart_episode_called = 0
+    global episodes_since_last_restart
+    episodes_since_last_restart += 1
+    if episodes_since_last_restart >= RESTART_AFTER_EPISODES:
+        episodes_since_last_restart = 0
         close_dota_client()
         close_steam_client()
 
@@ -73,7 +83,7 @@ def restart_game():
 
     # Enter the restart command
     gui.press('\\', pause=PAUSE)
-    gui.typewrite('restart', interval=INTERVAL)
+    gui.typewrite('restart', interval=TYPEWRITE_INT)
     gui.press('enter', pause=PAUSE)
     gui.press('\\', pause=PAUSE)
 
@@ -89,7 +99,7 @@ def restart_game():
 
     # Start the game right away
     gui.press('\\', pause=PAUSE)
-    gui.typewrite('dota_start_game', interval=INTERVAL)
+    gui.typewrite('dota_start_game', interval=TYPEWRITE_INT)
     gui.press('enter')
     gui.press('\\', pause=PAUSE)
 
@@ -99,10 +109,8 @@ def close_steam_client():
         return
     _focus_steam_window()
 
-    # Click on the steam menu option
-    gui.click(x=101, y=51, pause=PAUSE)
-    # Pick the exit option
-    gui.click(x=101, y=199, pause=PAUSE)
+    gui.click(x=STEAM_MENU['x'], y=STEAM_MENU['y'], pause=PAUSE)
+    gui.click(x=STEAM_EXIT['x'], y=STEAM_EXIT['y'], pause=PAUSE)
     time.sleep(30)
 
 
@@ -111,24 +119,21 @@ def close_dota_client():
         return
     _focus_dota_window()
 
-    # Bring up the menu
-    gui.click(x=256, y=256, pause=2*PAUSE)
+    gui.click(x=INGAME_MENU_BUTTON['x'], y=INGAME_MENU_BUTTON['y'], pause=2*PAUSE)
     # Disconnect
-    gui.click(x=RIGHT_BOT_BUTTON_X, y=RIGHT_BOT_BUTTON_Y, pause=2*PAUSE)
-    # Confirm it
-    gui.click(x=585, y=585, pause=4*PAUSE)
-    # Exit
-    gui.click(x=1022, y=256, pause=2*PAUSE)
-    # Confirm it and wait for complete closure
-    gui.click(x=580, y=568, pause=PAUSE)
+    gui.click(x=RIGHT_BOT_BUTTON['x'], y=RIGHT_BOT_BUTTON['y'], pause=2*PAUSE)
+    gui.click(x=INGAME_CONFIRM_BUTTON['x'], y=INGAME_CONFIRM_BUTTON['y'], pause=4*PAUSE)
+    gui.click(x=EXIT_BUTTON['x'], y=EXIT_BUTTON['y'], pause=2*PAUSE)
+    gui.click(x=MENU_CONFIRM_BUTTON['x'], y=MENU_CONFIRM_BUTTON['y'], pause=PAUSE)
+    # Wait for complete closure
     time.sleep(30)
 
 
 def calibrate_dota_client():
     gui.press('\\', pause=PAUSE)
-    gui.typewrite('sv_cheats 1', interval=INTERVAL)
+    gui.typewrite('sv_cheats 1', interval=TYPEWRITE_INT)
     gui.press('enter', pause=PAUSE)
-    gui.typewrite('host_timescale 5', interval=INTERVAL)
+    gui.typewrite('host_timescale 5', interval=TYPEWRITE_INT)
     gui.press('enter', pause=PAUSE)
     gui.press('\\', pause=PAUSE)
 
@@ -148,12 +153,12 @@ def _focus_steam_window():
         if window_info.find('N/A') != -1:
             window_id = window_info[:10]
             _run_cmd('wmctrl -i -a ' + window_id)
-    time.sleep(DURATION)
+    time.sleep(0.25)
 
 
 def _focus_dota_window():
     _run_cmd('wmctrl -a "Dota 2"')
-    time.sleep(DURATION)
+    time.sleep(0.25)
 
 
 def _run_cmd(cmd):
