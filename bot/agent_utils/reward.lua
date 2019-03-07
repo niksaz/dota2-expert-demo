7520df3e63a78dca5e284f8947dbb0511b5bf501
niksaz <nikitasazanovich@gmail.com>
2019-03-08 00:00:11 +0300

    Reward is given for attacking.

--- bot/agent_utils/reward.lua
+++ bot/agent_utils/reward.lua
@@ -107,23 +107,51 @@ function Reward.is_near_ally_tower()
 end
 
 local last_attack_time = agent:GetLastAttackTime()
-local last_attack_target_hero = false
 
 function Reward.get_reward(wrong_action)
+--    local my_health = get_my_health()
+--    local my_kills = get_my_kills()
+--    local enemy_health = get_enemy_health()
+--    local enemy_tower_health = get_enemy_tower_health()
+--    local ally_tower_health = get_ally_tower_health()
+--    local hits = get_last_hits()
+
+--    local reward = -(my_deaths - last_deaths) * 10000 -- deaths
+--            + (my_kills - last_kills) * 1000000 -- kills
+--            + max(enemy_tower_health - last_enemy_tower_health, 0) * 5 * is_near_enemy_tower() -- enemy tower
+--            - max(ally_tower_health - last_ally_tower_health, 0)
+--            + max(enemy_health - last_enemy_health, 0) * 100 * recently_damaged_enemy()
+--            - max(my_health - last_my_health, 0) * 100
+--            + max(hits - last_hits, 0) * 10000
+--            - get_distance_to_tower_punishment() / 200
+--            - wrong_action * 30
+
     local reward = 0
 
+--    if Reward.is_near_ally_tower() == 1 then
+--        reward = reward + 100
+--    end
+
     local attack_time = agent:GetLastAttackTime()
-    if attack_time ~= nil and (last_attack_time == nil or attack_time > last_attack_time) and last_attack_target_hero then
+    if attack_time ~= nil and (last_attack_time == nil or attack_time > last_attack_time) then
         reward = reward + 1
     end
     last_attack_time = attack_time
 
-    local attack_target = agent:GetAttackTarget()
-    if attack_target ~= nil and attack_target == get_enemy() then
-        last_attack_target_hero = true
-    else
-        last_attack_target_hero = false
-    end
+--  if wrong_action == 1 then
+--      reward = reward - 0.1
+--  end
+
+--  local deaths = get_my_deaths()
+--  reward = reward - (deaths - last_deaths) * 100
+--  last_deaths = deaths
+
+--    last_enemy_tower_health = enemy_tower_health
+--    last_ally_tower_health = ally_tower_health
+--    last_kills = my_kills
+--    last_hits = hits
+--    last_my_health = my_health
+--    last_enemy_health = enemy_health
 
     return reward
 end
